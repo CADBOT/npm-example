1f9123e06cb689921b0540150d591a6ca71bc650
tmp <tmp@gmail.com>
2019-09-12 16:04:06 -0700

    add webpack and webpack babel-loader

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+/*
+function Person(name) {
+  this.name = name;
+}
+
+var n = Person('pete');
+console.log(name);
+
 const _ = require('lodash')
 
 const main = () => {
@@ -5,3 +13,23 @@ const main = () => {
 }
 
 main()
+*/
+/*
+import * as lib from './lib'
+
+console.log(lib.sum(2, 2));
+console.log(lib.subtract(2, 2));
+console.log(lib.divide(2, 2));
+*/
+import _ from 'lodash'
+console.log(_.random())
+
+import { subtract, sum } from './lib' // tree shake*
+//const { subtract, sum } = require('./lib') // require extra config to treeshake
+
+console.log(sum(1, 2))
+console.log(subtract(1, 2))
+
+import Dog from './dog'
+const myDog = new Dog('echo');
+myDog.bark();
--- src/ex.js
+++
@@ -1,2 +0,0 @@
-var name = 'charlie'
-console.log(name)
